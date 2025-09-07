ece10068df31895b2d313b41c078efb5914ac834
Ariel França <ariel.franca@hotmail.com>
2025-09-06 22:18:50 -0300

    rename

---
+++ src/main/java/barber/model/User.java
@@ -0,0 +1,29 @@
+package barber.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Integer id;
+
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+
+}
